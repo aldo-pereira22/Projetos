098733c528807e8097192e9ad2cecc1cd50d9527
aldo-pereira22 <aldopereira.gyn@gmail.com>
2020-07-29 19:56:42 -0300

    update create-products

--- Angular/Crud/frontend/src/app/directives/for.directive.ts
+++ Angular/Crud/frontend/src/app/directives/for.directive.ts
@@ -1,27 +1,27 @@
-import { Directive, Input, ViewContainerRef, TemplateRef } from '@angular/core';
+
+import {
+  Directive,
+  OnInit,
+  Input,
+  ViewContainerRef,
+  TemplateRef,
+} from "@angular/core";
 
 @Directive({
-  selector: '[myFor]'
+  selector: "[myFor]",
 })
 export class ForDirective implements OnInit {
-
-  @Input('myForEm') numbers: number[]
-
+  @Input("myForEm") numbers: number[];
 
   constructor(
-    private container: ViewContainerRef, private template: TemplateRef<any>
-  
-  ) {
-
-  }
-
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+  ) {}
 
   ngOnInit(): void {
-
-    for(let number of this.numbers){
-      this.container.createEmbeddedView(this.template, { $implicit: number })
+    for (let number of this.numbers) {
+      this.container.createEmbeddedView(
+        this.template, { $implicit : number });
     }
-
-
   }
 }
